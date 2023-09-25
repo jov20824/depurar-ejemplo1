de353118a9871aca288e6110c51987f398dcd1dc
Alejandro Blanco <jov20824@educastur.es>
2023-09-25 13:53:19 +0200

    Arreglado

--- js/ejemplo1.js
+++ js/ejemplo1.js
@@ -11,9 +11,12 @@
 var nombres=[];
 
 var NUMERO_NOMBRES=10;
-
-
-
+var lista;
+for (var i=0;i<10;i++){
+    nombres[i] = prompt("Nombre");
+    lista += nombres[i]+" ";
+}
+console.warn(nombres);
 
 /* AVISOS (WARNING) */
 console.warn("Mensaje warn");
